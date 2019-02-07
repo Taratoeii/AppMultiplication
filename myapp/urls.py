d3a927420269d61e197f154041cd6a3b6e3ab616
Warakorn <teay_cute@hotmail.com>
2019-02-07 15:41:45 +0700

    Add Inputtype doing function

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:id>/', views.Cal, name='Calculate'),
+    path('<int:id>/', views.Cal, name='Cal'),
+    path('input/', views.InputNum, name='InputNum'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 
 def Cal(request, id):
-    id = str(id) + str(id) + str(id)
-    return HttpResponse(id)
+    # id = str(id) + str(id) + str(id)
+    id_text = str(id)
+    context = {
+        'name': "what",
+    }
+    return render(request, 'Show.html',context)
+
+def InputNum(request):
+    return HttpResponseRedirect(reverse('polls:Cal'))
