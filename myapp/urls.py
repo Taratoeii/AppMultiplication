d2e4a8d5aeb93b4f4fdda93e57f6d474e5ec8695
Warakorn <teay_cute@hotmail.com>
2019-02-14 14:01:27 +0700

    start keep static

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
-
+app_name = 'app'
 urlpatterns = [
-    path('<int:id>/', views.Cal, name='Cal'),
-    path('input/', views.InputNum, name='InputNum'),
+    path('<int:id>/', views.Show, name='Show'),
+    path('', views.InputNum, name='InputNum'),
+    path('mul/', views.mul, name='mul'),
+    path('keep/', views.keep, name='keep')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,37 @@
 from django.http import HttpResponse
-from django.template import loader
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-def Cal(request, id):
+def Show(request, id):
     # id = str(id) + str(id) + str(id)
-    id_text = str(id)
+    id_text = str(id) * 3
     context = {
-        'name': "what",
+        'num': id_text
     }
-    return render(request, 'Show.html',context)
+    return render(request, 'threenum.html',context)
 
 def InputNum(request):
-    return HttpResponseRedirect(reverse('polls:Cal'))
+    return render(request,'Input.html')
+
+def mul(request):
+    list_num = []
+    for i in range(1,13):
+        num = int(request.POST['number']) * i
+        list_num.append(num)
+    context = {
+        'id': request.POST['number'],
+        'num': list_num
+    }
+    return render(request, 'Mul.html', context)
+
+def keep(request):
+    listnum = []
+    for i in range(1,13):
+        listnum.append(i)
+    context = {
+        'id' : request.POST['number'] ,
+        'num' : listnum
+    }
+    return render(request, 'keep.html', context)
+
+
